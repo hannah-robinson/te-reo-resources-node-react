e099abff69c5aa69eaf99187803ae92378ab6917
Hannah Robinson <hannah.robinson.git@gmail.com>
2022-04-23 22:58:40 +1200

    Add nav using React Router and create Redux store

--- client/components/Header/Header.jsx
+++ client/components/Header/Header.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import style from './Header.module.scss'
 
 function Header() {
-  return <div>Hello from Header!</div>
+  return (
+    <div>
+      <header className="primary-header flex">
+        <div className="logo">
+          <a href="/">
+            <i className="fa-solid fa-comment-dots"></i>
+          </a>
+        </div>
+        <nav>
+          <ul
+            id="primary-navigation"
+            className="primary-navigation underline-indicators flex nav-link"
+          >
+            <li key="home">
+              <Link to={'/'}>All Resources</Link>
+            </li>
+            {/* <li>
+              <a href="/free">Free Resources</a>
+            </li> */}
+          </ul>
+        </nav>
+      </header>
+      <h1>Te Reo Māori Learning Resources</h1>
+    </div>
+  )
 }
 
 export default Header
--- client/index.js
+++ client/index.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { render } from 'react-dom'
-// import {Provider} from 'react-redux'
-// import {store} from './store'
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
+import store from './store'
 
 import App from './components/App/App'
 
 document.addEventListener('DOMContentLoaded', () => {
   render(
-    // <Provider store={store}>
-    <App />,
-    // </Provider>,
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>,
     document.getElementById('app')
   )
 })
