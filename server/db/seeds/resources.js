5bf21bd5a9bb36e43beac5dcbdd351e44c91624a
Hannah Robinson <hannah.robinson.git@gmail.com>
2022-04-23 15:18:03 +1200

    Create db seed

---
+++ server/db/seeds/resources.js
@@ -0,0 +1,69 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = async function (knex) {
+  // Deletes ALL existing entries
+  await knex('resources').del()
+  await knex('resources').insert([
+    {
+      id: 1,
+      name: 'Te Aka Māori Dictionary',
+      url: 'https://maoridictionary.co.nz/',
+      image: '/images/te-aka.png',
+      description:
+        'This is a great dictionary. It provides meanings, example sentences and audio so you can hear how to pronounce the word. There are also app versions of the dictionary that you can buy for iOS and Android.',
+      cost: 'free',
+      language_level: 'beginner+',
+      medium: 'website',
+      free: true,
+    },
+    {
+      id: 2,
+      name: 'Drops',
+      url: 'https://languagedrops.com/language/learn-maori',
+      image: '/images/drops.png',
+      description: 'Learn 2,000+ Māori words with Drops.',
+      cost: 'free or from NZ$9.33/month for premium',
+      language_level: 'beginner+',
+      medium: 'app',
+      free: false,
+    },
+    {
+      id: 3,
+      name: 'Toro Mai',
+      url: 'https://toromai.massey.ac.nz/',
+      image: '/images/toromai.png',
+      description:
+        'Learn Te Reo Māori and Tikanga Māori online for free with Massey University.',
+      cost: 'free',
+      language_level: 'beginner',
+      medium: 'online course',
+      free: true,
+    },
+    {
+      id: 4,
+      name: 'Te Karere (video)',
+      url: 'https://www.youtube.com/user/tekareremaorinews',
+      image: '/images/te-karere.png',
+      description:
+        'This channel is great for practising listening in te reo Māori. You will learn vocabulary related to current events. The video version can be helpful because the visuals can give you context to help your understanding. Some segments also have English subtitles.',
+      cost: 'free',
+      language_level: 'intermediate+',
+      medium: 'website',
+      free: true,
+    },
+    {
+      id: 4,
+      name: 'Te Karere (audio)',
+      url: 'https://soundcloud.com/tekarere',
+      image: '/images/te-karere-audio.png',
+      description:
+        'This channel is great for practising listening in te reo Māori. You will learn vocabulary related to current events. The audio version is great because you can listen while you are doing other things or as you go too sleep at night.',
+      cost: 'free',
+      language_level: 'intermediate+',
+      medium: 'website',
+      free: true,
+    },
+  ])
+}
