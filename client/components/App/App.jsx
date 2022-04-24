eb2eccbba265b011cc1d10206e46f2baaef5a386
Hannah Robinson <hannah.robinson.git@gmail.com>
2022-04-24 15:32:42 +1200

    Add home route

--- client/components/App/App.jsx
+++ client/components/App/App.jsx
@@ -1,18 +1,29 @@
 import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+// import { useDispatch, useSelector } from 'react-redux'
+import { Routes, Route } from 'react-router-dom'
+
+// import { fetchResources } from '../../actions'
 
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import ResourceList from '../ResourceList/ResourceList'
 
 function App() {
+  // const resources = useSelector((state) => state.resources)
+  // const dispatch = useDispatch()
+  // useEffect(() => {
+  //   dispatch(fetchResources())
+  // }, [])
   return (
     <>
-      <div>
-        <Header />
-        <ResourceList />
-        <Footer />
-      </div>
+      <Header />
+      <main className="container">
+        <Routes>
+          <Route path="/" element={<ResourceList />} />
+          {/* <Route path=":id" element={<Resource />} /> */}
+        </Routes>
+      </main>
+      <Footer />
     </>
   )
 }
