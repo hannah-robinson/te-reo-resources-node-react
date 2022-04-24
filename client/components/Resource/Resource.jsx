6f5dd8bd5a24de09ee5ca4e235237a3fa7f38741
Hannah Robinson <hannah.robinson.git@gmail.com>
2022-04-24 13:55:17 +1200

    Create React components to display fetched resources

--- client/components/Resource/Resource.jsx
+++ client/components/Resource/Resource.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
 
-import style from './Resource.module.scss'
+// import style from './Resource.module.scss'
 
-function Resource() {
-  return <div>Hello from Resource!</div>
+function Resource({
+  id,
+  resourceName,
+  image,
+  languageLevel,
+  medium,
+  description,
+  cost,
+  url,
+}) {
+  return (
+    <div className="card" key={id}>
+      <h2>{resourceName}</h2>
+      <img src={image} />
+      <p>{description}</p>
+      <div className="card-details">
+        <p>
+          <em>
+            <strong>Language level:</strong> {languageLevel}
+          </em>
+        </p>
+        <p>
+          <em>
+            <strong>Type of resource:</strong> {medium}
+          </em>
+        </p>
+        <p>
+          <em>
+            <strong>Cost:</strong> {cost}
+          </em>
+        </p>
+      </div>
+      <a href={url}>
+        Visit site <i className="fa-solid fa-arrow-up-right-from-square"></i>
+      </a>
+      <div className="crud-links">
+        <a href={`/${id}/edit`}>Edit</a> <a href={`/${id}/delete`}>Delete</a>
+      </div>
+    </div>
+  )
 }
 
 export default Resource
--- client/components/ResourceList/ResourceList.jsx
+++ client/components/ResourceList/ResourceList.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
-import style from './ResourceList.module.scss'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { fetchResources } from '../../actions'
+
+// import style from './ResourceList.module.scss'
 
 import Resource from '../Resource/Resource'
 
 function ResourceList() {
+  const resources = useSelector((state) => state.resources)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(fetchResources())
+  }, [])
   return (
     <div className="container grid">
-      Hello from ResourceList!
-      <Resource />
+      {resources.map((resource) => (
+        <Resource
+          key={resource.id}
+          resourceName={resource.resourceName}
+          image={resource.image}
+          languageLevel={resource.languageLevel}
+          medium={resource.medium}
+          cost={resource.cost}
+          url={resource.url}
+          id={resource.id}
+        />
+      ))}
     </div>
   )
 }
