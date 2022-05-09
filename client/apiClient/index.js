1effda6c4b4856b0435b2542aecac4fa28d30e05
Hannah Robinson <hannah.robinson.git@gmail.com>
2022-05-09 21:16:46 +1200

    Refactor: move apiClient into its own folder

---
+++ client/apiClient/index.js
@@ -0,0 +1,5 @@
+import * as resourcesApi from './resources'
+
+export default {
+  ...resourcesApi,
+}
---
+++ client/apiClient/resources.js
@@ -0,0 +1,38 @@
+import request from 'superagent'
+
+const resourcesUrl = '/api/v1/resources/'
+
+// Read
+export function getResources() {
+  return request.get(resourcesUrl).then((response) => response.body.resources)
+}
+
+export function getResource(id) {
+  return request.get(resourcesUrl + id).then((response) => response.body)
+}
+
+// Create
+export function postResource(resource) {
+  return request
+    .post(resourcesUrl)
+    .send(resource)
+    .then((response) => response.body)
+}
+
+// Delete
+export function deleteResource(id) {
+  return request
+    .del(resourcesUrl + id)
+    .send(id)
+    .then((response) => response.body.resources)
+}
+
+// Update
+export function updateResource(id, resource) {
+  return request
+    .patch(resourcesUrl + id)
+    .send(resource)
+    .then((res) => {
+      return res.body
+    })
+}
