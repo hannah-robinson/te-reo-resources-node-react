cf4c7fd8f18a3041813782b9cbf9118d152b73b7
Hannah Robinson <hannah.robinson.git@gmail.com>
2022-05-12 21:55:38 +1200

    Fix: wrap <form> in a conditional so it will have an error when the 'resource' variable is undefined while the fetchResources() thunk is loading

--- client/components/EditResource/EditResource.jsx
+++ client/components/EditResource/EditResource.jsx
@@ -60,69 +60,71 @@ function EditResource(props) {
 
   return (
     <div className='container'>
-      <form>
-        <h2>Edit resource</h2>
-        <p>
-          <label htmlFor='resourceName'>Name: </label>
-          <input
-            name='resourceName'
-            onChange={handleChange}
-            defaultValue={resource.resourceName}
-            value={formData.resourceName}
-          ></input>
-        </p>
-        <p>
-          <label htmlFor='description'>Description: </label>
-          <input
-            name='description'
-            onChange={handleChange}
-            value={formData.description}
-          ></input>
-        </p>
-        <p>
-          <label htmlFor='url'>
-            URL – copy and past a link to the resource's own website:{' '}
-          </label>
-          <input
-            name='url'
-            onChange={handleChange}
-            value={formData.url}
-          ></input>
-        </p>
-        <p>
-          <label htmlFor='image'>Image file path: </label>
-          <input
-            name='image'
-            onChange={handleChange}
-            value={formData.image}
-          ></input>
-        </p>
-        <p>
-          <label htmlFor='languageLevel'>Language level: </label>
-          <input
-            name='languageLevel'
-            onChange={handleChange}
-            value={formData.languageLevel}
-          ></input>
-        </p>
-        <p>
-          <label htmlFor='medium'>Type of resource: </label>
-          <input
-            name='medium'
-            onChange={handleChange}
-            value={formData.medium}
-          ></input>
-        </p>
-        <p>
-          <label htmlFor='cost'>Price: </label>
-          <input
-            name='cost'
-            onChange={handleChange}
-            value={formData.cost}
-          ></input>
-        </p>
-        <button onClick={handleSubmit}>Save</button>
-      </form>
+      {resource && (
+        <form>
+          <h2>Edit resource</h2>
+          <p>
+            <label htmlFor='resourceName'>Name: </label>
+            <input
+              name='resourceName'
+              onChange={handleChange}
+              defaultValue={resource.resourceName}
+              value={formData.resourceName}
+            ></input>
+          </p>
+          <p>
+            <label htmlFor='description'>Description: </label>
+            <input
+              name='description'
+              onChange={handleChange}
+              value={formData.description}
+            ></input>
+          </p>
+          <p>
+            <label htmlFor='url'>
+              URL – copy and past a link to the resource's own website:{' '}
+            </label>
+            <input
+              name='url'
+              onChange={handleChange}
+              value={formData.url}
+            ></input>
+          </p>
+          <p>
+            <label htmlFor='image'>Image file path: </label>
+            <input
+              name='image'
+              onChange={handleChange}
+              value={formData.image}
+            ></input>
+          </p>
+          <p>
+            <label htmlFor='languageLevel'>Language level: </label>
+            <input
+              name='languageLevel'
+              onChange={handleChange}
+              value={formData.languageLevel}
+            ></input>
+          </p>
+          <p>
+            <label htmlFor='medium'>Type of resource: </label>
+            <input
+              name='medium'
+              onChange={handleChange}
+              value={formData.medium}
+            ></input>
+          </p>
+          <p>
+            <label htmlFor='cost'>Price: </label>
+            <input
+              name='cost'
+              onChange={handleChange}
+              value={formData.cost}
+            ></input>
+          </p>
+          <button onClick={handleSubmit}>Save</button>
+        </form>
+      )}
     </div>
   )
 }
